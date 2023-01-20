2844b14d96eadcc0d65608d8d26e3b5e71a23f93
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2023-01-20 12:42:40 +0300

    add Mult

---
+++ Mult.java
@@ -0,0 +1,12 @@
+public class Multy extends ArithmeticOperation {
+    Calculations calc = new Calculations();
+
+    public Rational multiply(Rational one, Rational two) {
+        numerator = calc.getNum(one) * calc.getNum(two);
+        denominator = calc.getDenom(one) * calc.getDenom(two);
+        gcdResult = calc.gcd(numerator, denominator);
+        numerator /= gcdResult;
+        denominator /= gcdResult;
+        return new Rational(numerator, denominator);
+    }
+}

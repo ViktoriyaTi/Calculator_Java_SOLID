1aa2c9cbfbeef5c0e3645533879fd4fe578a807e
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2023-01-20 12:43:35 +0300

    create presenter-sub,sum,mult,div

--- Presenter.java
+++ Presenter.java
@@ -4,7 +4,11 @@ import java.util.logging.*;
 
 public class Presenter {
     View view = new View();
-    Calculator calc = new Calculator();
+    Calculations calc = new Calculations();
+    Sum sum = new Sum();
+    Sub sub = new Sub();
+    Multy multy = new Multy();
+    Div div = new Div();
     Scanner in = new Scanner(System.in);
     Logger logger;
     FileHandler fh;
@@ -21,22 +25,22 @@ public class Presenter {
         Rational inputTwo = view.getValueNumber(view.s2);
         if (choice == 1) {
             this.logger.log(Level.INFO, "Выбрана операция суммирования");
-            Rational resultSum = calc.sum(inputOne, inputTwo);
+            Rational resultSum = sum.getSum(inputOne, inputTwo);
             view.printSum(inputOne, inputTwo, resultSum);
             // this.logger.log(Level.INFO, "Результат суммирования = " + resultSum);
 
         } else if (choice == 2) {
             this.logger.log(Level.INFO, "Выбрана операция вычитания");
-            Rational resultSub = calc.subtract(inputOne, inputTwo);
+            Rational resultSub = sub.subtract(inputOne, inputTwo);
             view.printSub(inputOne, inputTwo, resultSub);
 
         } else if (choice == 3) {
             this.logger.log(Level.INFO, "Выбрана операция умножения");
-            Rational resultMult = calc.multiply(inputOne, inputTwo);
+            Rational resultMult = multy.multiply(inputOne, inputTwo);
             view.printMult(inputOne, inputTwo, resultMult);
         } else {
             this.logger.log(Level.INFO, "Выбрана операция деления");
-            Rational resultDiv = calc.divide(inputOne, inputTwo);
+            Rational resultDiv = div.divide(inputOne, inputTwo);
             view.printDiv(inputOne, inputTwo, resultDiv);
 
         }
